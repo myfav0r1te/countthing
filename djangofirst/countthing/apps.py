1b8db1be81c0bc1475f77f75ffd21a5de2438184
eunseo <rttwths@gmail.com>
2020-05-09 07:08:16 +0900

    first commit of countthinig

---
+++ djangofirst/countthing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CountthingConfig(AppConfig):
+    name = 'countthing'
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\박은서\desktop\counter\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
